a799101e2c606f28d7d862b93663ee25557bc436
Nick Amoscato <nick@amoscato.com>
2020-06-07 07:36:20 -0400

    Try alternative Exif reader

--- src/image.ts
+++ src/image.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
-import {ExifData, ExifImage} from 'exif'
 import Source from 'tinify/lib/tinify/Source'
 import tinify from 'tinify'
 import bytes from 'bytes'
+import ExifReader from 'exifreader'
 
 export default class Image {
   private source?: Source
@@ -11,41 +11,31 @@ export default class Image {
   constructor(private readonly filename: string) {}
 
   async compress(): Promise<void> {
-    await this.logInfo('Before')
+    this.logInfo('Before')
 
     this.source = tinify.fromFile(this.filename)
 
-    return this.source
-      .toFile(this.filename)
-      .then(async () => this.logInfo('After'))
+    return this.source.toFile(this.filename).then(() => {
+      this.logInfo('After')
+    })
   }
 
   getFilename(): string {
     return this.filename
   }
 
-  private async getExif(): Promise<ExifData> {
-    return new Promise((resolve, reject) => {
-      new ExifImage({image: this.filename}, function(error, exifData) {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(exifData)
-        }
-      })
-    })
-  }
-
   private getSize(): number {
     return fs.statSync(this.filename).size
   }
 
-  private async logInfo(message: string): Promise<void> {
-    return this.getExif().then(exifData => {
-      const size = bytes.format(this.getSize())
-      const exif = JSON.stringify(exifData, null, 4)
+  private logInfo(message: string): void {
+    const size = bytes.format(this.getSize())
+    const exif = JSON.stringify(
+      ExifReader.load(fs.readFileSync(this.filename)),
+      null,
+      4
+    )
 
-      core.debug(`[${this.filename}] ${message}: ${size}\n${exif}`)
-    })
+    core.debug(`[${this.filename}] ${message}: ${size}\n${exif}`)
   }
 }
