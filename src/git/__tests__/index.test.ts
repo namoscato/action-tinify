2534df2a0126e3d2f2d747cbd42079a590839a53
Nick Amoscato <nick@amoscato.com>
2021-03-20 17:44:39 -0400

    Remove pull_request_target from supported events (#208)

--- src/git/__tests__/index.test.ts
+++ src/git/__tests__/index.test.ts
@@ -65,54 +65,52 @@ describe('Git', () => {
       })
     })
 
-    for (const eventName of ['pull_request', 'pull_request_target']) {
-      describe(eventName, () => {
-        beforeEach(() => {
-          scope.get('/repos/OWNER/REPO/pulls/1/files').reply(200, [
-            {
-              id: 1,
-              status: 'added'
-            },
-            {
-              id: 2,
-              status: 'removed'
-            },
-            {
-              id: 3,
-              status: 'modified'
-            }
-          ])
-        })
-
-        test('should fetch files', async () => {
-          const files = await new Git({
-            token: 'TOKEN',
-            context: ({
-              eventName,
-              payload: {
-                number: 1
-              },
-              repo: {
-                owner: 'OWNER',
-                repo: 'REPO'
-              }
-            } as unknown) as Context
-          }).getFiles()
-
-          expect(files).toEqual([
-            {
-              id: 1,
-              status: 'added'
-            },
-            {
-              id: 3,
-              status: 'modified'
-            }
-          ])
-
-          expect(scope.isDone()).toBe(true)
-        })
+    describe('pull_request', () => {
+      beforeEach(() => {
+        scope.get('/repos/OWNER/REPO/pulls/1/files').reply(200, [
+          {
+            id: 1,
+            status: 'added'
+          },
+          {
+            id: 2,
+            status: 'removed'
+          },
+          {
+            id: 3,
+            status: 'modified'
+          }
+        ])
       })
-    }
+
+      test('should fetch files', async () => {
+        const files = await new Git({
+          token: 'TOKEN',
+          context: ({
+            eventName: 'pull_request',
+            payload: {
+              number: 1
+            },
+            repo: {
+              owner: 'OWNER',
+              repo: 'REPO'
+            }
+          } as unknown) as Context
+        }).getFiles()
+
+        expect(files).toEqual([
+          {
+            id: 1,
+            status: 'added'
+          },
+          {
+            id: 3,
+            status: 'modified'
+          }
+        ])
+
+        expect(scope.isDone()).toBe(true)
+      })
+    })
   })
 })
