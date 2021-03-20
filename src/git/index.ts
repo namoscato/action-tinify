a2789b2210710d260b704083cdb834add32024d0
Nick Amoscato <nick@amoscato.com>
2021-03-20 16:22:46 -0400

    Add log

--- src/git/index.ts
+++ src/git/index.ts
@@ -30,7 +30,6 @@ export default class Git {
           const ref = commit.id
 
           info(`[${this.context.eventName}] Fetching files for commit ${ref}`)
-
           filesPromises.push(
             this.getCommitFiles({
               ...this.context.repo,
@@ -78,6 +77,7 @@ export default class Git {
     if (isPullRequestContext(this.context)) {
       remote = this.context.payload.pull_request.head.repo.git_url
 
+      info('Detecting detached state')
       if (await this.isDetached()) {
         info('Checking out branch from detached state')
         await exec('git', [
